360a7bc5c16b3864fa67728dd1bc650ac8044cc2
JPFurlanis <antonialifurlan@gmail.com>
2024-10-24 00:47:19 -0300

    Ui update 3

--- menu.gd
+++ menu.gd
@@ -1,20 +1,9 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_button_pressed() -> void:
+	$VBoxContainer/Button.hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scence/main.tscn")
-
-
-func _on_quit_pressed() -> void:
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
 	get_tree().quit()
--- scrpits/main.gd
+++ scrpits/main.gd
@@ -3,7 +3,10 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
-
+func _on_Start_Game_pressed():
+	$VBoxContainer/Button.hide()
 
 func _on_button_2_pressed() -> void:
 	pass # Replace with function body.
+func _on_Quit_pressed():
+	get_tree().quit()
